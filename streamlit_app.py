3e7c574b8fbd86e6a3c96c5562f2aeaa9032b435
DLW <166925365+dwalls225@users.noreply.github.com>
2024-12-21 09:24:26 -0800

    Update streamlit_app.py
    fixed a comment error

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('Search_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('Search_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 #Convert Snowpark DF to Pandas DF so we can use LOC function
